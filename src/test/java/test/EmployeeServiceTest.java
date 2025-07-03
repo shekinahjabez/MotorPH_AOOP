e729e9983b9b4aaed4f4243b8feaa60ad4fe2c2e
Shekinah Jabez <162029890+shekinahjabez@users.noreply.github.com>
2025-07-03 14:59:33 +0800

    Update EmployeeServiceTest.java

--- src/test/java/test/EmployeeServiceTest.java
+++ src/test/java/test/EmployeeServiceTest.java
@@ -39,9 +39,9 @@ public class EmployeeServiceTest {
     @BeforeAll
     public static void setup() throws SQLException {
         System.out.println("--- Starting EmployeeServiceTest: Setting up database connection and service. ---");
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/motorph_db";
         String user = "postgres";
-        String password = "postgres";
+        String password = "postgres"; // change password
         connection = DriverManager.getConnection(url, user, password);
         employeeService = new EmployeeService(connection);
         System.out.println("Database connection and service initialized.");
