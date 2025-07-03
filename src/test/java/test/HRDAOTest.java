81bdac47e5ec3f414a27696fefd15fd132c9802b
Shekinah Jabez <162029890+shekinahjabez@users.noreply.github.com>
2025-07-03 23:43:22 +0800

    Update HRDAOTest.java

--- src/test/java/test/HRDAOTest.java
+++ src/test/java/test/HRDAOTest.java
@@ -37,9 +37,9 @@ public class HRDAOTest {
     @BeforeAll
     public static void setup() throws SQLException {
         System.out.println("--- Starting HRServiceTest: Setting up database connection and service. ---");
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/motorph_db";
         String user = "postgres";
-        String password = "sj";
+        String password = "postgres";
         connection = DriverManager.getConnection(url, user, password);
         hrService = new HRDAO(connection);
         System.out.println("Database connection and service initialized.");
